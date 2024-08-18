98c98bc9fcdad1be1da1be94ad985c6e4045e3f2
Nahaq789 <naha022677@icloud.com>
2024-08-19 07:29:29 +0900

    fix: password hash

--- src/domain/entity/auth_user.rs
+++ src/domain/entity/auth_user.rs
@@ -1,8 +1,6 @@
-use password_hash::PasswordHasher;
-use pbkdf2::{
-    password_hash::{rand_core::OsRng, SaltString},
-    Params, Pbkdf2,
-};
+use anyhow::Result;
+use password_hash::{Error as PasswordHashError, PasswordHasher};
+use pbkdf2::{password_hash::SaltString, Params, Pbkdf2};
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
 
@@ -13,18 +11,23 @@ pub struct AuthUser {
 }
 
 impl AuthUser {
-    fn hash_password(&self, password: &[u8], salt: &String) -> Result<String, password_hash::Error> {
-        let salt_string = SaltString::from_b64(&salt)?;
-                let params: Params = pbkdf2::Params {
+    pub fn new(name: String, password: &str, salt: &String) -> Result<Self, PasswordHashError> {
+        let salt = SaltString::from_b64(&salt)?;
+        let hashed_password = Self::hash_password(password.as_bytes(), &salt)?;
+
+        Ok(Self {
+            name,
+            password: hashed_password,
+        })
+    }
+    fn hash_password(password: &[u8], salt: &SaltString) -> Result<String, PasswordHashError> {
+        let params: Params = pbkdf2::Params {
             rounds: (10000),
             output_length: (32),
         };
 
-        let hash = Pbkdf2
-            .hash_password_customized(password, None, None, params, &salt_string)
+        Pbkdf2
+            .hash_password_customized(password, None, None, params, salt)
             .map(|hash| hash.to_string())
-            .unwrap_or_else(|_| "Error: Failed to hash password".to_string());
-
-        Ok(hash)
     }
 }
