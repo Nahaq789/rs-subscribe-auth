badcb9621e0c8b08beefcf0dd94f1a233edc93fc
Nahaq789 <naha022677@icloud.com>
2024-08-23 19:45:39 +0900

    fix: config

--- src/adapter/aws/config.rs
+++ src/adapter/aws/config.rs
@@ -1,6 +1,5 @@
 use std::env;
-
-use anyhow::Ok;
+use thiserror::Error;
 
 #[derive(Debug, Clone)]
 pub struct AwsConfig {
@@ -8,13 +7,25 @@ pub struct AwsConfig {
     client_id: String,
 }
 
-impl AwsConfig {
-    fn from_lambda_env() -> Result<Self, env::VarError> {
-        let config = AwsConfig {
-            user_pool_id: env::var("AWS_USER_POOL_ID")?,
-            client_id: env::var("AWS_CLIENT_ID")?,
-        };
+#[derive(Error, Debug)]
+pub enum AwsConfigError {
+    #[error("Environment variable not found: {0}")]
+    EnvVarNotFound(String),
+}
 
-        Ok(config.)
+impl AwsConfig {
+    pub fn new (user_pool_id: String, client_id: String) -> Self {
+        AwsConfig {
+            user_pool_id, client_id
+        }
+    }
+
+    fn from_lambda_env() -> Result<Self, AwsConfigError> {
+        let user_pool_id = env::var("AWS_USER_POOL_ID").map_err(|_| AwsConfigError::EnvVarNotFound("AWS_USER_POOL_ID".to_string()))?;
+        let client_id = env::var("AWS_CLIENT_ID").map_err(|_| AwsConfigError::EnvVarNotFound("AWS_CLIENT_ID".to_string()))?;
+
+        let config = AwsConfig::new(user_pool_id, client_id);
+
+        Ok(config)
     }
 }
