0c831e6e851d6158b192e87634d2a38450193cae
Ran Mouri <naha022677@icloud.com>
2024-08-24 18:50:11 +0900

    feat: lambdaと開発時にことなるenvを取得

--- src/adapter/aws/config.rs
+++ src/adapter/aws/config.rs
@@ -1,10 +1,11 @@
+use dotenv::dotenv;
 use std::env;
 use thiserror::Error;
 
 #[derive(Debug, Clone)]
 pub struct AwsConfig {
-    user_pool_id: String,
-    client_id: String,
+    pub user_pool_id: String,
+    pub client_id: String,
 }
 
 #[derive(Error, Debug)]
@@ -21,7 +22,8 @@ impl AwsConfig {
         }
     }
 
-    pub fn from_lambda_env() -> Result<Self, AwsConfigError> {
+    pub fn get_env_value() -> Result<Self, AwsConfigError> {
+        dotenv().ok();
         let user_pool_id = env::var("AWS_USER_POOL_ID")
             .map_err(|_| AwsConfigError::EnvVarNotFound("AWS_USER_POOL_ID".to_string()))?;
         let client_id = env::var("AWS_CLIENT_ID")
--- src/application/auth/auth_service.rs
+++ src/application/auth/auth_service.rs
@@ -1,6 +1,9 @@
 use axum::async_trait;
 
-use crate::{domain::entity::auth_user::AuthUser, exception::auth_error::AuthError};
+use crate::{
+    adapter::aws::config::AwsConfig, domain::entity::auth_user::AuthUser,
+    exception::auth_error::AuthError,
+};
 
 #[async_trait]
 pub trait AuthService: Send + Sync {
@@ -18,6 +21,9 @@ impl AuthServiceImpl {
 #[async_trait]
 impl AuthService for AuthServiceImpl {
     async fn auth_by_cognito(&self, auth: AuthUser) -> Result<(), AuthError> {
+        let unko = AwsConfig::get_env_value().unwrap();
+        println!("{}", unko.user_pool_id);
+
         Ok(())
     }
 }
