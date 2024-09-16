3c0f78987f03965fff82ea7da106b65fd70d711e
Ran Mouri <naha022677@icloud.com>
2024-09-17 08:17:30 +0900

    feat: email and password validation

--- src/domain/entity/auth_user.rs
+++ src/domain/entity/auth_user.rs
@@ -1,4 +1,6 @@
+use regex::Regex;
 use serde::{Deserialize, Serialize};
+use crate::domain::exception::auth_domain_exception::AuthDomainException;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AuthUser {
@@ -9,12 +11,31 @@ pub struct AuthUser {
 }
 
 impl AuthUser {
-    pub fn new(user_id: String, email: String, password: String, verify_code: String) -> Self {
-        AuthUser {
+    pub fn new(user_id: String, email: String, password: String, verify_code: String) -> Result<Self, AuthDomainException> {
+        Ok(AuthUser {
             user_id,
-            email,
-            password,
+            email: Self::set_email(email)?,
+            password: Self::set_password(password)?,
             verify_code,
+        })
+    }
+
+    fn set_email(email: String) -> Result<String, AuthDomainException> {
+        let regex = Regex::new(r"^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")
+            .map_err(|_| AuthDomainException::RegexCompilationFailed)?;
+        if regex.is_match(&email) {
+            Ok(email)
+        } else {
+            Err(AuthDomainException::ValidateFailed)
+        }
+    }
+
+    fn set_password(password: String) -> Result<String, AuthDomainException> {
+        let regex = Regex::new(r"").map_err(|_| AuthDomainException::RegexCompilationFailed)?;
+        if regex.is_match(&password) {
+            Ok(password)
+        } else {
+            Err(AuthDomainException::ValidateFailed)
         }
     }
 }
---
+++ src/domain/exception/auth_domain_exception.rs
@@ -0,0 +1,10 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum AuthDomainException {
+    #[error("Validate Error")]
+    ValidateFailed,
+
+    #[error("Regex Compile Failed")]
+    RegexCompilationFailed
+}
