cbbadae5ee076922fbbddf93a1c19b6ebc34fac5
Ran Mouri <naha022677@icloud.com>
2024-09-21 07:25:35 +0900

    test: add error response test #9

--- src/presentation/dto/error_response.rs
+++ src/presentation/dto/error_response.rs
@@ -8,10 +8,25 @@ pub struct ErrorResponse {
 }
 
 impl ErrorResponse {
-    pub fn new(status_code: StatusCode, message: String) -> Self {
+    pub fn new(status_code: StatusCode, message: &str) -> Self {
         ErrorResponse {
             status_code: status_code.as_u16(),
-            message,
+            message: message.into(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_error_response_create_success() {
+        let status_code = StatusCode::BAD_REQUEST;
+        let message = String::from("hoge");
+        let result = ErrorResponse::new(status_code, &message);
+
+        assert_eq!(result.status_code, StatusCode::BAD_REQUEST);
+        assert_eq!(result.message, message)
     }
 }
